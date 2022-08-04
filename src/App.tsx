8d7873ac4c48500a2dcb5f55d475b16683681dc8
edyta555 <edyta.badowska555@gmail.com>
2022-08-04 17:24:40 +0200

    feat: fetch data regarding the doctors from JSON Server

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
 import styles from "./app.module.scss";
 
 function App() {
-  return <div className={styles.App}><p>test</p></div>;
+  const [doctorsData, setDoctorsData] = useState([]);
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/doctors");
+        const listItems = await response.json();
+        console.log(listItems);
+        setDoctorsData(listItems);
+      } catch (err) {
+        setDoctorsData([]);
+        console.log(err);
+      }
+    };
+
+    (() => fetchItems())();
+    console.log("doctorsData,", doctorsData);
+  }, []);
+
+  return <div className={styles.App}></div>;
 }
 
 export default App;
